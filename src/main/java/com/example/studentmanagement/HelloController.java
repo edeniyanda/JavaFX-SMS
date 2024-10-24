6862aa60fd2ae4b485927efd0088b53ec5cfcc58
Eden Iyanda <edeniyanda@gmail.com>
2024-10-24 02:20:59 +0100

    Update: Included functionality to switch between Scene

--- src/main/java/com/example/studentmanagement/HelloController.java
+++ src/main/java/com/example/studentmanagement/HelloController.java
@@ -4,6 +4,10 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
@@ -11,6 +15,9 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
 
@@ -61,6 +68,20 @@ public class HelloController {
 
     // Counter to generate unique IDs
     private int studentIdCounter = 1;
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+
+    public void switchToHomeScene(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("welcome-view.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
 
     @FXML
     public void initialize() {
